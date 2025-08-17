14783626dbd971ad7b4c0a52a40c4bd46265cee2
iamtruptimane <iamtruptimane@gmail.com>
2025-08-17 11:35:48 +0530

    added

--- Day-1/ec2.tf
+++ Day-1/ec2.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
   alias = "us-east"
 }
+provider "aws" {
+  region = "ap-south-1"
+  alias = "ap-south"
+}
 
 provider "aws" {
   region = "ap-south-1"
@@ -16,16 +20,16 @@ resource "aws_instance" "web-us-east" {
   key_name = "local-key"
   vpc_security_group_ids = ["sg-0bc72fa56351a2ff9"]
   tags = {
-    Name = "webserver"
+    Name = "web-USA"
   }
 }
 resource "aws_instance" "web-ap-south" {
-  provider = aws.us-east
-  ami = "ami-0de716d6197524dd9"
+  provider = aws.ap-south
+  ami = "ami-0144277607031eca2"
   instance_type = "t3.micro"
-  key_name = "local-key"
-  vpc_security_group_ids = ["sg-0bc72fa56351a2ff9"]
+  key_name = "mumbai-key"
+  vpc_security_group_ids = ["sg-03be1aec650d2bbee"]
   tags = {
-    Name = "webserver"
+    Name = "web-mumbai"
   }
 }
